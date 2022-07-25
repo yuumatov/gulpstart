576ee1a88bd8062b73a2870f829089c45cce2b41
Yuumatov <yuumatov@yandex.ru>
2022-07-25 19:31:53 +0300

    Правки

--- gulp/task/ftp.js
+++ gulp/task/ftp.js
@@ -18,20 +18,3 @@ export const ftp = () => {
     .pipe(ftpConnect.newer(`${app.path.ftp}/${app.path.rootFolder}`))
     .pipe(ftpConnect.dest(`${app.path.ftp}/${app.path.rootFolder}`));
 };
-
-export const ftpcss = () => {
-  configFTP.log = util.log;
-  const ftpConnect = vinylFTP.create(configFTP);
-  return app.gulp
-    .src(`${app.path.buildFolder}/css/*.*`, { buffer: false })
-    .pipe(
-      app.plugins.plumber(
-        app.plugins.notify.onError({
-          title: 'FTP',
-          message: 'Error: <%= error.message %>',
-        })
-      )
-    )
-    .pipe(ftpConnect.newer(`${app.path.ftp}/${app.path.rootFolder}/css/`))
-    .pipe(ftpConnect.dest(`${app.path.ftp}/${app.path.rootFolder}/css/`));
-};
