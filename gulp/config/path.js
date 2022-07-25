c652f7fa9419691e5979e9e19fe8f349a649d660
Yuumatov <yuumatov@yandex.ru>
2022-07-25 11:51:16 +0300

    Инициализация репозитория

---
+++ gulp/config/path.js
@@ -0,0 +1,36 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = './dist';
+const srcFolder = './src';
+
+export const path = {
+	build: {
+		html: `${buildFolder}/`,
+		css: `${buildFolder}/css/`,
+		js: `${buildFolder}/js/`,
+		img: `${buildFolder}/img/`,
+		fonts: `${buildFolder}/fonts/`,
+		favicon: `${buildFolder}/favicons/`
+	},
+	src: {
+		html: `${srcFolder}/*.html`,
+		scss: `${srcFolder}/scss/main.scss`,
+		js: `${srcFolder}/js/main.js`,
+		img: [`${srcFolder}/img/**/*.{jpg,jpeg,png,webp,svg,gif}`, `!${srcFolder}/img/sprite/**/*.*`],
+		sprite: `${srcFolder}/img/sprite/*.svg`,
+		favicon: `${srcFolder}/favicons/**/*.*`,
+	},
+	watch: {
+		html: `${srcFolder}/**/*.html`,
+		scss: `${srcFolder}/scss/**/*.scss`,
+		js: `${srcFolder}/js/**/*.js`,
+		img: [`${srcFolder}/img/**/*.{jpg,jpeg,png,webp,svg,gif}`, `!${srcFolder}/img/sprite/**/*.*`],
+		sprite: `${srcFolder}/img/sprite/*.svg`
+	},
+	clean: buildFolder,
+	srcFolder: srcFolder,
+	buildFolder: buildFolder,
+	rootFolder: rootFolder,
+	ftp: `projects`
+}
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import './_vendors.js';
+import './_functions.js';
+import './_components.js';
+
+console.log('Java Script Ready');
